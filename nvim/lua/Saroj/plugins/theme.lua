13256a00d91f7c8df56840408c1fd46652c91709
Saroj <sarojregmi.official@gmail.com>
2025-11-05 19:55:15 +0545

    feat: Added a time-interval based theme switcher

--- nvim/lua/Saroj/plugins/theme.lua
+++ nvim/lua/Saroj/plugins/theme.lua
@@ -1,6 +1,43 @@
+local utils = require("Saroj.utils")
+local set_theme = utils.set_theme
+local set_interval = utils.set_interval
+local clear_interval = utils.clear_interval
+
+local timer = nil
+
+local change_theme_according_to_time = function()
+	local hour = tonumber(os.date("%H"))
+
+	if hour < 12 then
+		set_theme("vague", true)
+	elseif hour > 13 and hour < 16 then
+		set_theme("yugen", true)
+	else
+    vim.cmd([[colorscheme rasmus]])
+	end
+end
+
 return {
-	"vague2k/vague.nvim",
+	"kvrohit/rasmus.nvim",
+	dependencies = {
+		"vague2k/vague.nvim",
+		"bettervim/yugen.nvim",
+	},
+
+	init = function()
+		timer = set_interval(30, function()
+			vim.schedule(function()
+				change_theme_according_to_time()
+			end)
+		end)
+	end,
+
 	config = function()
-		vim.cmd.colorscheme("vague")
+		change_theme_according_to_time()
+		vim.api.nvim_create_autocmd("UILeave", {
+			callback = function()
+				clear_interval(timer)
+			end,
+		})
 	end,
 }
